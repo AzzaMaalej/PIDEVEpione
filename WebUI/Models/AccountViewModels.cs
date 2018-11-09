30766c16ee56c46e6cf74cc6a8241ae9f0691d14
AzzaMaalej <36265670+AzzaMaalej@users.noreply.github.com>
2018-11-09 20:18:12 +0100

    Add files via upload

---
+++ WebUI/Models/AccountViewModels.cs
@@ -0,0 +1,51 @@
+﻿using Domain;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebUI.Models
+{
+    public class AccountViewModels
+    {
+
+        public class RegisterViewModel
+        {
+            [Required]
+            [Display(Name = "Username")]
+            public string UserName { get; set; }
+
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+
+            [Required]
+            [Display(Name = "Account Type")]
+            public EAccountType AccountType { get; set; }
+        }
+
+        public class LoginViewModel
+        {
+            [Required]
+            [Display(Name = "UserName")]
+            public string Username { get; set; }
+
+           
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            [Display(Name = "Remember me?")]
+            public bool RememberMe { get; set; }
+        }
+    }
+}
