bc10c3ce9c63bdb61872a012f762daed5cd4e70c
AzzaMaalej <36265670+AzzaMaalej@users.noreply.github.com>
2018-11-09 19:00:32 +0100

    Add files via upload

---
+++ GUI/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Domain;
+using System.Threading.Tasks;
+using Data;
+
+namespace GUI
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            PiContext ctx = new PiContext();
+          /*  User u = new User { Email = "emel.garouachi@esprit.tn",
+             Password= "1234",
+            adress = "1 rue de sfax",
+            UserName="emel",
+            gender=Gender.
+            female,
+            lastName="Garouachi",
+            birthDate=DateTime.Now};*/
+        //    ctx.Users.Add(u);
+         //   ctx.SaveChanges();
+        }
+    }
+}
