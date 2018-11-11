90422a301ee85160567882e50bc20c9e08b4a384
AzzaMaalej <36265670+AzzaMaalej@users.noreply.github.com>
2018-11-12 00:37:07 +0100

    Azza calender

---
+++ WebUI/Controllers/EspaceMedecin/CalenderController.cs
@@ -0,0 +1,74 @@
+﻿using Data;
+using DHTMLX.Common;
+using DHTMLX.Scheduler;
+using DHTMLX.Scheduler.Data;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Controllers.EspaceMedecin
+{
+    public class CalenderController : Controller
+    {
+        // GET: Calender
+        public ActionResult Index()
+        {
+            var scheduler = new DHXScheduler(this);
+            scheduler.Skin = DHXScheduler.Skins.Flat;
+
+            scheduler.Config.first_hour = 6;
+            scheduler.Config.last_hour = 20;
+            scheduler.EnableDynamicLoading(SchedulerDataLoader.DynamicalLoadingMode.Month);
+            scheduler.LoadData = true;
+            scheduler.EnableDataprocessor = true;
+            return View(scheduler);
+        }
+        PiContext db = new PiContext();
+        public ContentResult Data(DateTime from, DateTime to)
+        {
+            var apps = db.Disponibilities.Where(e => e.StartDate < to && e.EndDate >= from).ToList();
+            return new SchedulerAjaxData(apps);
+        }
+        public ActionResult Save(int? id, FormCollection actionValues)
+        {
+            var action = new DataAction(actionValues);
+
+            try
+            {
+                var changedEvent = DHXEventsHelper.Bind<Disponibility>(actionValues);
+                switch (action.Type)
+                {
+                    case DataActionTypes.Insert:
+                        db.Disponibilities.Add(changedEvent);
+                        break;
+                    case DataActionTypes.Delete:
+                        db.Entry(changedEvent).State = EntityState.Deleted;
+                        break;
+                    default:// "update"  
+                        db.Entry(changedEvent).State = EntityState.Modified;
+                        break;
+                }
+                db.SaveChanges();
+                action.TargetId = changedEvent.Id;
+            }
+            catch (Exception a)
+            {
+                action.Type = DataActionTypes.Error;
+            }
+
+            return (new AjaxSaveResponse(action));
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
