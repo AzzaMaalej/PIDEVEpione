2cc9ebd0e2b9624d5a4252acc701fd722be56ce5
cheikhnadesri <cheikhnadesry@gmail.com>
2018-11-14 16:42:33 +0100

    Add files via upload

---
+++ WebUI/Controllers/Doctolib/RssController.cs
@@ -0,0 +1,25 @@
+﻿using Domain;
+using ServicePattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebUI.Models;
+
+namespace WebUI.Controllers.Doctolib
+{
+    public class RssController : Controller
+    {
+        // GET: Rss
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult RssReader()
+        {
+            return View(WebUI.Models.Doctolib.RssReader.GetRssFeed());
+        }
+       
+    }
+}
