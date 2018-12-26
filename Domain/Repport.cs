830c67cd52fc76c1a0b61ad047a7434feb4f2643
ivanpro2018 <ivanlandry.onanabakout@esprit.tn>
2018-12-26 15:21:12 +0100

    Modif_repport

--- Domain/Repport.cs
+++ Domain/Repport.cs
@@ -9,8 +9,17 @@ namespace Domain
     public class Repport
     {
         public int RepportId { get; set; }
-        public String ReppotName { get; set; }
-        public Doctor doctor { get; set; }
+        public string RepportName { get; set; }
+        public DateTime RepportDate { get; set; }
+        public string PatientName { get; set; }
+        public string DiseaseDeclared { get; set; }
+        public string Symptoms { get; set; }
+        public string Abnormalities { get; set; }
+        public string ImageDisease { get; set; }
+        public string RepportContent { get; set; }
+        public string Diagnostic { get; set; }
+        public string ReferentDoctor { get; set; }
+        public Doctor Doctor { get; set; }
         //public virtual Course course { get; set; }
 
     }
