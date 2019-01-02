91475673a48f2d24223631e1d423254a89abd85e
ivanpro2018 <ivanlandry.onanabakout@esprit.tn>
2019-01-02 19:43:07 +0100

    Expositions des méthodes login et register

---
+++ WebUI/Controllers/Users/AccountController.cs
@@ -0,0 +1,147 @@
+﻿using Domain;
+using Microsoft.AspNet.Identity.Owin;
+using Service.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using static WebUI.Models.AccountViewModels;
+
+namespace WebUI.Controllers.Users
+{
+    public class AccountController : ApiController
+    {
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
+        public AccountController()
+        {
+        }
+
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.Current.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        // POST: /Account/Login
+
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        [System.Web.Http.Route("api/Login")]
+        public async Task<IHttpActionResult> Login(LoginViewModell model)
+        {
+            var result = await SignInManager.PasswordSignInAsync(model.Username, model.PasswordHash, model.RememberMe, shouldLockout: false);
+            switch (result)
+            {
+
+                case SignInStatus.Success:
+                    return Ok("ConnexionSuccess");
+                case SignInStatus.Failure:
+                default:
+                    ModelState.AddModelError("", "Invalid login attempt.");
+                    return Ok("ConnexionFailed");
+            }
+        }
+
+
+
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        [System.Web.Http.Route("api/Register")]
+        public async Task<IHttpActionResult> Register(RegisterViewModell model)
+        {
+            // return Ok(model.AccountType);
+            switch (model.AccountType)
+            {
+
+                case EAccountType.Patient:
+                    {
+                        Patient v = new Patient
+                        {
+                            UserName = (model.UserName),
+                            Email = model.Email,
+                            Address = model.Address,
+                            FirstName = model.FirstName,
+                            LastName = model.LastName,
+                            BirthDate = model.BirthDate,
+                            ImageName = model.ImageName,
+                            PhoneNumber = model.PhoneNumber,
+                            Gender = model.Gender,
+
+                        };
+                        var results = await UserManager.CreateAsync(v, model.PasswordHash);
+                        if (results.Succeeded)
+                        {
+                            //result = await UserManager.AddToRoleAsync(user.Id, model.Cv);
+                            await SignInManager.SignInAsync(v, isPersistent: false, rememberBrowser: false);
+                            return Ok("Patient added !");
+                        }
+                    }
+                    break;
+                case EAccountType.Doctor:
+                    {
+                        Doctor res = new Doctor
+                        {
+                            UserName = model.UserName,
+                            Email = model.Email,
+                            Address = model.Address,
+                            FirstName = model.FirstName,
+                            LastName = model.LastName,
+                            BirthDate = model.BirthDate,
+                            ImageName = model.ImageName,
+                            PhoneNumber = model.PhoneNumber,
+                            Gender = model.Gender,
+                            Speciality = model.Speciality
+
+                        };
+                        if (res.ImageName == null) { res.ImageName = "default-user-image.png"; }
+                        var results = await UserManager.CreateAsync(res, model.PasswordHash);
+                        if (results.Succeeded)
+                        {
+                            //result = await UserManager.AddToRoleAsync(user.Id, model.Cv);
+                            await SignInManager.SignInAsync(res, isPersistent: false, rememberBrowser: false);
+                            return Ok("Docteur added !");
+                        }
+
+                    }
+                    break;
+            }
+
+            return Ok("Fail !");
+
+        }
+
+    }
+}
--- WebUI/Models/AccountViewModels.cs
+++ WebUI/Models/AccountViewModels.cs
@@ -43,6 +43,45 @@ namespace WebUI.Models
             public SpecialityEnum Speciality { get; set; }
         }
 
+        public class RegisterViewModell
+        {
+            [Required]
+            [Display(Name = "Username")]
+            public string UserName { get; set; }
+            public String FirstName { get; set; }
+            public String LastName { get; set; }
+            [DataType(DataType.Date)]
+            public DateTime BirthDate { get; set; }
+            public String Address { get; set; }
+            public Gender Gender { get; set; }
+
+            public String ImageName { get; set; }
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+            
+            //[StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+            [Required]
+            [DataType(DataType.Password)]
+            [Display(Name = "PasswordHash")]
+            public string PasswordHash { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+            public string PhoneNumber { get; set; }
+            [Required]
+            [Display(Name = "Account Type")]
+            public EAccountType AccountType { get; set; }
+            [Display(Name = "Speciality")]
+            public SpecialityEnum Speciality { get; set; }
+            [Display(Name = "AccessFailedCount")]
+            public int AccessFailedCount { get; set; }
+
+        }
+
         public class LoginViewModel
         {
             [Required]
@@ -53,6 +92,20 @@ namespace WebUI.Models
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
+
+            [Display(Name = "Remember me?")]
+            public bool RememberMe { get; set; }
+        }
+        public class LoginViewModell
+        {
+            [Required]
+            [Display(Name = "UserName")]
+            public string Username { get; set; }
+
+           
+            [DataType(DataType.Password)]
+            [Display(Name = "PasswordHash")]
+            public string PasswordHash { get; set; }
 
             [Display(Name = "Remember me?")]
             public bool RememberMe { get; set; }
